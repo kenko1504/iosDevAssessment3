1c652e8910a188d316015d1d6c69bd5b07552b66
kenko1504 <kenkowatanabe1504@gmail.com>
2025-05-02 19:34:00 +1000

    mvvc folder added

---
+++ IosAssessment3/Views/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  cardView.swift
+//  IosAssessment3
+//
+//  Created by Kenji Watanabe on 2/5/2025.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
