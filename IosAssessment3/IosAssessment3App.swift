78d049724efabefdf4864e06c0accc59b4b446b5
kenko1504 <kenkowatanabe1504@gmail.com>
2025-05-02 14:22:31 +1000

    inital project added

---
+++ IosAssessment3/IosAssessment3App.swift
@@ -0,0 +1,17 @@
+//
+//  IosAssessment3App.swift
+//  IosAssessment3
+//
+//  Created by Kenji Watanabe on 2/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct IosAssessment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IosAssessment3Tests/IosAssessment3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  IosAssessment3Tests.swift
+//  IosAssessment3Tests
+//
+//  Created by Kenji Watanabe on 2/5/2025.
+//
+
+import Testing
+@testable import IosAssessment3
+
+struct IosAssessment3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
