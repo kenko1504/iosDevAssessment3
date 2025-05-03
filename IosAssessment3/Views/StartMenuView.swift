e5f5911f5bb5fbd821a6f13ef465f866940ce3fe
kenko1504 <kenkowatanabe1504@gmail.com>
2025-05-03 20:21:41 +1000

    added score table

--- IosAssessment3/Views/StartMenuView.swift
+++ IosAssessment3/Views/StartMenuView.swift
@@ -11,12 +11,24 @@ struct StartMenuView: View {
     @State private var selectedDifficulty = "Easy"
     let difficulties = ["Easy", "Medium", "Hard"]
     
+    //variables for gamescore table
+    //first three is describes the difficulties
+    @State var data = ["Easy", "Medium", "Hard"] + (1...15).map { "\($0)" }
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
         NavigationStack{
             VStack {
                 Text("Concentration card game")
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                 Spacer()
+                Text("Top 5 scores")
+                displayGameScoreTable()
                 HStack{
                     Text("Difficulty:")
                     Picker("Difficulty", selection:$selectedDifficulty){
@@ -29,7 +41,29 @@ struct StartMenuView: View {
                 NavigationLink("Start Game", destination: CardGameView())
             }
         }
+        .onAppear{
+            updateGameScoreTable()
+        }
 
+    }
+    
+    func displayGameScoreTable() -> some View{
+        LazyVGrid(columns: columns, spacing: 10) {
+            ForEach(data, id: \.self) { item in
+                Text("\(item)")
+                    .frame(maxWidth: .infinity, minHeight: 50)
+                    .background(Color.blue.opacity(0.2))
+                    .cornerRadius(8)
+            }
+        }
+    }
+    
+    func updateGameScoreTable() {
+        data[3] = "test"
+    }
+    
+    func addGameScores(){
+        
     }
 }
 
