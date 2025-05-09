26cc32f23058363c9c59b2694ca3a9bcde9ffdb8
kenko1504 <kenkowatanabe1504@gmail.com>
2025-05-09 20:03:10 +1000

    firebase sdk

---
+++ IosAssessment3/Views/SettingView.swift
@@ -0,0 +1,25 @@
+//
+//  TimerView.swift
+//  IosAssessment3
+//
+//  Created by Kenji Watanabe on 3/5/2025.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+
+
+    var body: some View {
+        VStack{
+            Text("test")
+        }
+
+    }
+    
+}
+
+#Preview {
+    SettingView()
+}
+
